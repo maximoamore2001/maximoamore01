dedbd0d1e396868d0635be3c8478bdc8522e35fa
maximoamore2001 <maxiamorerc88@gmail.com>
2023-05-31 10:51:57 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/498d1e9147.js" crossorigin="anonymous"></script>
+    <title>portfolioV2</title>
+</head>
+
+<body>
+    <div class="container-fluid wrapper">
+        <div class="">
+            <header>
+                <nav class="navbar row">
+                    <div class="menu text-center col-12 p-2">
+                        <a href="#">home</a><a href="#">contacto</a>
+                    </div>
+                </nav>
+            </header>
+            <main class="main row">
+                <div class="info-content col-sm-8 col-12 text-center">
+                    <h1>i'm <span id="span-maximo">Máximo</span> Amore</h1>
+                    <p>un apasionado desarrollador web
+                        dedicado a crear sitios web funcionales y
+                        atractivos. Con mis conocimientos en lenguajes de
+                        programación y tecnologías web, le ofrezco paginas
+                        innovadoras y de alta calidad a mis clientes.
+                    </p>
+                    <a download href="#">
+                        <button class="download-cv">Download CV <i class="bi bi-download"></i></button>
+                    </a>
+                </div>
+                <div class="col-sm-4 text-center p-3 col-12">
+                    <img src="img/30_5_2023 07_26_33.png" alt="maximo amore picture">
+                </div>
+            </main>
+        </div>
+    </div>
+    <div>
+        <div class="container-fluid">
+            <div class="about row">
+                <div class="col-12 col-sm-5 pic d-none d-sm-none d-md-block">
+                    <img src="img/DSC_0348.jpeg" alt="maximo amore portrait">
+                </div>
+                <div class="about-info text-center col-sm-7 mt-5 col-12">
+                    <h2>About Me</h2>
+                    <p>Soy un desarrollador web apasionado por la tecnología y entusiasta de la creatividad. Me encanta crear sitios web y aplicaciones, y siempre estoy buscando formas de mejorar mis habilidades en esta área. Aunque no tengo mucha experiencia, estoy emocionado por aprender y enfrentar nuevos desafíos en el campo del desarrollo web.Además de mi amor por la tecnología, el deporte ocupa un lugar importante en mi vida. Dedico tiempo a mantenerme en forma y participar en diferentes actividades deportivas. Creo en la importancia del equilibrio entre el trabajo y el bienestar físico, y el deporte me ayuda a mantenerme centrado y motivado en todas las áreas de mi vida.
+                        Mi personalidad se caracteriza por ser educado y alegre. Disfruto colaborando con otros, compartiendo mis conocimientos y apoyando a quienes me rodean. Aunque mi experiencia puede ser limitada, mi pasión y determinación me impulsan a seguir creciendo como desarrollador web. Estoy emocionado por lo que el futuro me depara en este campo y estoy comprometido a alcanzar mis metas y convertirme en un profesional exitoso en el mundo del desarrollo web.
+                    </p>
+                    <button class="download-cv"><i class="bi bi-github"></i></button>
+                    <button class="download-cv"><i class="bi bi-linkedin"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="skills">
+        <div class="container">
+            <div class="text-center col-sm-12 col-12">
+                <h2 class="pc d-none d-sm-none d-md-block">Herramientas <i class="bi bi-tools"></i></h2>
+                <h2 class="smartphone d-block d-sm-block d-md-none">Herramientas <i class="bi bi-tools"></i></h2>
+            </div>
+            <div class="row bloque-programa pt-5">
+                <div class="col-sm-3 col-12 text-center">
+                    <p>HTML</p>
+                    <img src="img/html5-logo-html-logo-0.png" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>CSS</p>
+                    <img src="img/css.png" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>JAVASCRIPT</p>
+                    <img src="img/javascript.jpeg" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>GIT</p>
+                    <img src="img/git.png" alt="">
+                </div>
+            </div>
+            <div class="row bloque-programa">
+                <div class="col-sm-3 col-12 text-center">
+                    <p>PHP</p>
+                    <img src="img/hwUcGZ41_400x400.jpg" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>LARAVEL</p>
+                    <img src="img/LALARAVELlogo-laravel-1024.png" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>APACHE</p>
+                    <img src="img/apache0020_999_1622694565_apache_256.png" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center" id="mercado-pago">
+                    <p>MERCADO PAGO</p>
+                    <img src="img/gUjuxmmd_400x400.jpg" alt="">
+                </div>
+            </div>
+            <div class="row bloque-programa">
+                <div class="col-sm-3 col-12 text-center">
+                    <p>MYSQL</p>
+                    <img src="img/hwUcGZ41_400x400.jpg" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>BOOTSTRAP</p>
+                    <img src="img/LALARAVELlogo-laravel-1024.png" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>REACT.JS</p>
+                    <img src="img/apache0020_999_1622694565_apache_256.png" alt="">
+                </div>
+                <div class="col-sm-3 col-12 text-center">
+                    <p>JQUERY</p>
+                    <img src="img/gUjuxmmd_400x400.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ new.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/498d1e9147.js" crossorigin="anonymous"></script>
+    <title>index</title>
+</head>
+
+<body>
+    <div class="container">
+        <div id="navegacion">
+            <header class="row">
+                <div class="col-sm-12 col-12">
+                    <nav class="navbar">
+                        <ul>
+                            <a href="">
+                                <li>home</li>
+                            </a>
+                            <a href="">
+                                <li>contacto</li>
+                            </a>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
+        <div id="home">
+            <div class="row">
+                <div class="col-sm-12">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
