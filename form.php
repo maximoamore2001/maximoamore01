91020cc576e9565ff19313ac958a9693bee1552f
maximoamore2001 <maxiamorerc88@gmail.com>
2023-06-04 10:44:07 -0300

    form funcionando

--- form.php
+++ form.php
@@ -8,7 +8,7 @@ $header = header("location: new.html");
 //como me va a llegar el cuerpo del  mail a mi , lo que la gente escribio en el form
 $mensaje = "este mensaje fue enviado por " . $nombre;
 $mensaje .= "su e-mail es: " . $mail;
-$mensaje .= "mensaje: " . $_POST["mensaje"];
+$mensaje .= "mensaje: " . $_POST["mensajes"];
 $mensaje .= "enviado el " . date("d/m/Y", time());
 
 $para = "cybertechrosario@gmail.com";
@@ -16,7 +16,7 @@ $asunto = "este mail fue enviado de maximoamore";
 
 //funcion mail:
 //a quien le mando este mail
-mail($para , $asunto , utf8_decode($mensaje));
+mail($para , $asunto ,$mensaje);
 
 //redireccionar al haber enviado el form
 header("location: new.html");
