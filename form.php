76cd569f913f3ffbad4b418828099f20241d3a35
maximoamore2001 <maxiamorerc88@gmail.com>
2023-06-05 10:53:00 -0300

    guardar

--- form.php
+++ form.php
@@ -6,17 +6,17 @@ $mensaje = $_POST["mensajes"];
 $header = header("location: new.html");
 
 //como me va a llegar el cuerpo del  mail a mi , lo que la gente escribio en el form
-$mensaje = "este mensaje fue enviado por " . $nombre;
-$mensaje .= "su e-mail es: " . $mail;
-$mensaje .= "mensaje: " . $_POST["mensajes"];
-$mensaje .= "enviado el " . date("d/m/Y", time());
+$mensaje = "este mensaje fue enviado por " . $nombre . "." . "\n";
+$mensaje .= "su e-mail es: " . $mail . "." . "\n";
+$mensaje .= "mensaje: " . $_POST["mensajes"] . "." . "\n";
+$mensaje .= "enviado el " . date("d/m/Y", time()) . "." . "\n";
 
-$para = "cybertechrosario@gmail.com";
+$para = "maximoamoreDB@gmail.com";
 $asunto = "este mail fue enviado de maximoamore";
 
 //funcion mail:
 //a quien le mando este mail
-mail($para , $asunto ,$mensaje);
+mail($para, $asunto ,$mensaje);
 
 //redireccionar al haber enviado el form
 header("location: new.html");
